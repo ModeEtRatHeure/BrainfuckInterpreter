e62b33c6b0ead4489a5d043de1dc1886256fb6b6
Replit user <>
2023-05-10 08:57:57 +0000

    interpreter is now fully working

--- js/brainfuckInterpreter.js
+++ js/brainfuckInterpreter.js
@@ -1,15 +1,24 @@
 const cols = 150;
 const mem_size = 1000;
-const memory = [];
-for (i = 0; i < mem_size; i++){
-  memory.push(0);
-}
+let memory = [];
+const openedBracketMemoryPosition = {};
+const closedBracketMemoryPosition = {};
 let pointer = 0;
 
 newBfCodeInput(true);
 
+initMemory();
+
+function initMemory(){
+  memory = []
+  for (i = 0; i < mem_size; i++){
+    memory.push(0);
+  }
+}
+
 function newBfCodeInput(isFirst) {
   const content = "<p>C:\\> bf -interpreter</p><textarea autofocus=\"true\" class=\"terminal-lines\" autocorrect=\"off\" spellcheck=\"false\" rows=\"100\" cols=\"" + cols + "\"/>";
+  
   if(isFirst){
     document.getElementById("terminal").innerHTML += content;
   }else{
@@ -18,6 +27,7 @@ function newBfCodeInput(isFirst) {
   let terminalLine = document.getElementsByClassName("terminal-lines")[document.getElementsByClassName("terminal-lines").length - 1];
   terminalLine.focus();
   terminalLine.addEventListener("keydown", run);
+  initMemory();
 }
 
 function run(event) {
@@ -25,19 +35,25 @@ function run(event) {
     const textarea = document.getElementsByClassName("terminal-lines")[document.getElementsByClassName("terminal-lines").length - 1];
     textarea.autofocus = false;
     textarea.blur();
-    textarea.setAttribute("rows", Math.floor(textarea.value.length / (cols + 5)));
+    textarea.setAttribute("rows", Math.ceil(textarea.value.length / textarea.cols));
     textarea.disabled = true;
-    readCode(textarea.value);
+    readCode(textarea.value.replace("\n", ""));
   }
 }
 
 
 function readCode(code) {
-  code.split("").forEach(e => interpret(e));
+  for(let i = 0; i < code.length; i++){
+    try{
+      i = interpret(code[i], i, code);
+    }catch(error){
+      continue;
+    }
+  }
   newBfCodeInput(false);
 }
 
-function interpret(char){
+function interpret(char, index, code){
   switch(char){
     case ">":
       pointer += 1;
@@ -46,24 +62,31 @@ function interpret(char){
       pointer -= 1;
       break;
     case ".":
-      writeTerminal("<p>" + memory[pointer] + "</p>");
+      writeTerminal("<p>" + String.fromCharCode(memory[pointer]) + "</p>");
       break;
     case ",":
       break;
     case "+":
-      memory[pointer] += 1;
+      memory[pointer] = memory[pointer] < 255 ? memory[pointer] + 1 : 0;
       break;
     case "-":
-      memory[pointer] -= 1;
+      memory[pointer] = memory[pointer] > 0 ? memory[pointer] - 1 : 255;
       break;
     case "[":
+      if(memory[pointer] == 0){
+        return findCorrespondingClosedBracket(index, code);
+      }
       break;
     case "]":
+      if(memory[pointer] != 0){
+        return findCorrespondingOpenedBracket(index, code);
+      }
       break;
     default:
       console.info("Skipped an unknown char.");
       break;
   }
+  return index;
 }
 
 function writeTerminal(string) {
@@ -71,5 +94,44 @@ function writeTerminal(string) {
     document.getElementById("terminal").children.item(document.getElementById("terminal").children.length - 1).insertAdjacentHTML("afterend", string);
   }catch(error){
     document.getElementById("terminal").innerHTML += string;
+  }
+}
+
+function findCorrespondingClosedBracket(position, text){
+  let offset = 1;
+  let additionalOpenedBracket = 0;
+  while(true){
+    if(text[position + offset] == "]"){
+      if(additionalOpenedBracket == 0){
+        return position + offset;
+      }else{
+        additionalOpenedBracket -= 1;
+      }
+    }
+    if(offset > 1000){
+      break; //To stop infinite loop
+    }
+    if(text[position + offset] == "["){
+      additionalOpenedBracket += 1;
+    }
+    offset += 1;
+  }
+}
+
+function findCorrespondingOpenedBracket(position, text){
+  let offset = 1;
+  let additionalClosedBracket = 0;
+  while(true){
+    if(text[position - offset] == "["){
+      if(additionalClosedBracket == 0){
+        return position - offset;
+      }else{
+        additionalClosedBracket -= 1;
+      }
+    }
+    if(text[position - offset] == "]"){
+      additionalClosedBracket += 1;
+    }
+    offset += 1;
   }
 }
